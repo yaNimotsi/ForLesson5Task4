fc7dff651908cc1bf3f79ca7fd782f36a39fe0c2
Istomin.I.A <Istomin.I.A@Z4-ISTOMINIA>
2021-02-02 13:02:11 +0300

    Add project files.

---
+++ ConsoleApp7/Objects.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace ConsoleApp7
+{
+    enum ObjectTypes
+    {
+        folder,
+        file
+    }
+    class Objects
+    {
+        static string pathObject;
+        static string objectType;
+
+        public static string PathObject
+        {
+            get { return pathObject; }
+            set
+            {
+                pathObject = value;
+            }
+        }
+
+        public static string ObjectType
+        {
+            get { return objectType; }
+            set { objectType = GetTypeObject(value); }
+        }
+
+        public Objects()
+        {
+
+        }
+        /// <summary>
+        /// Определение типа найденного объекта
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        static string GetTypeObject(string value)
+        {
+            string extension = Path.GetExtension(value);
+            
+            if (extension == null) return ObjectTypes.folder.ToString();
+
+            return ObjectTypes.file.ToString();
+        }
+    }
+}
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFolder = @"D:\Текущие документы\Дыбрын земля площадок";
+
+            string[] AllSubFolders = GetAllFolder(pathFolder);
+
+            PrintFolderAndFiles(AllSubFolders);
+        }
+
+        static string[] GetAllFolder(string startFolderPath)
+        {
+            return Directory.GetDirectories(startFolderPath, "*", SearchOption.AllDirectories);
+        }
+
+        static string[] GetFilesInFolder(string folderPath)
+        {
+            string[] pathsToFiles = Directory.GetFiles(folderPath);
+
+            string[] nameFiles = new string[pathsToFiles.Length];
+            for (int i = 0; i< pathsToFiles.Length; i++)
+            {
+                nameFiles[i] = Path.GetFileName(pathsToFiles[i]);
+            }
+
+            return nameFiles;
+        }
+
+        static void PrintFolderAndFiles(string[] AllFolders)
+        {
+            for(int i = 0; i < AllFolders.Length; i++)
+            {
+                if (AllFolders[i] == null) continue;
+                else
+                {
+                    Console.WriteLine($"Текущая папка {AllFolders[i]}");
+                    string[] filesInFolder = GetFilesInFolder(AllFolders[i]);
+
+                    if (filesInFolder.Length > 0)
+                    {
+                        Console.WriteLine("Файлы в указанной папке:");
+                        Console.WriteLine();
+                        foreach (string str in filesInFolder) Console.WriteLine(str);
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("В указанной папке нет файлов!");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
